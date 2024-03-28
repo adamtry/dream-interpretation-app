694755129df13499b049593c8a6788fb4f203ad9
Adam <adam.tracy20@gmail.com>
2024-03-28 19:03:43 +0000

    Fix devcontainer and formatter

--- src/components/Form.js
+++ src/components/Form.js
@@ -19,12 +19,12 @@ const Form = ({onPress}) => {
           marginBottom: 10,
           padding: 8,
         }}
-        onChangeText={(text) => onChangeTitle(text)}
+        onChangeText={text => onChangeTitle(text)}
         value={title}
       />
       <TextInput
         style={{height: 40, borderColor: 'gray', borderWidth: 1, padding: 8}}
-        onChangeText={(text) => onChangeDescription(text)}
+        onChangeText={text => onChangeDescription(text)}
         value={description}
       />
       <Button title="Save Todo" onPress={handleSubmit} />
