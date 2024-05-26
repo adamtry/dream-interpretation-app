d7d443940e6f78086529b970bde2987dd337655b
Adam <adam.tracy20@gmail.com>
2024-05-26 00:19:57 +0000

    Add URL

--- src/types/Dream.ts
+++ src/types/Dream.ts
@@ -1,14 +1,13 @@
-import { User } from "./User";
 
 export interface Dream {
   id: string;
   title: string;
   description: string;
   date: string; // yyyy-mm-dd
-  user: User;
+  userId: string;
 }
 
-export interface DreamReq extends Omit<Dream, "id"> {}
+export interface DreamReq extends Omit<Dream, "id" | "userId"> {}
 
 export interface DreamUpdate extends Partial<Dream> {
   id: string;
