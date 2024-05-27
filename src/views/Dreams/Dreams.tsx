1ae3c4eac647d3bd7d8178935d0fd3bf07e70c0b
Adam <adam.tracy20@gmail.com>
2024-05-27 17:22:23 +0000

    Fix all dreams cache on edit and delete

--- src/views/Dreams/Dreams.tsx
+++ src/views/Dreams/Dreams.tsx
@@ -4,6 +4,9 @@ import {
   IonFabButton,
   IonHeader,
   IonIcon,
+  IonInfiniteScroll,
+  IonInfiniteScrollContent,
+  IonNote,
   IonRefresher,
   IonRefresherContent,
   IonTitle,
@@ -14,34 +17,79 @@ import { Dream } from "../../types/Dream";
 import { IonPage } from "@ionic/react";
 
 import { add } from "ionicons/icons";
-import { useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import useSWR, { mutate } from "swr";
 import { DREAMFLOW_API_URL, fetchUser, fetcher } from "../../data/DreamflowApi";
 import { DreamCard } from "./components/DreamCard";
 import { SearchBar } from "./components/SearchBar";
 
-export function MyDreams() {
-  const [textFilter, setTextFilter] = useState<string>("");
+interface DreamPageProps {
+  page: number;
+  pageSize: number;
+  setMorePagesExist: (morePagesExist: boolean) => void;
+}
+function DreamPage({ page, pageSize, setMorePagesExist }: DreamPageProps) {
+  const { data, error } = useSWR<Dream[]>(
+    `${DREAMFLOW_API_URL}/users/${fetchUser().uid}/dreams?page=${page}&pageSize=${pageSize}`,
+    fetcher,
+  );
 
-  function filterDreams(searchFilter: string, dreams: Dream[]) {
-    return dreams
-      .filter((dream) => {
-        var titleMatch = dream.title.toLowerCase().includes(searchFilter.toLowerCase());
-        var descriptionMatch = dream.description.toLowerCase().includes(searchFilter.toLowerCase());
-        return titleMatch || descriptionMatch;
-      })
-      .sort((a, b) => {
-        return new Date(a.date) < new Date(b.date) ? 1 : -1;
-      });
+  useEffect(() => {
+    if (data) setMorePagesExist(data.length > 0);
+  }, [data]);
+
+  if (error) return <div>Error loading dreams...</div>;
+  if (!data) return <div>Loading dreams...</div>;
+  if (data.length === 0) {
+    return (
+      <IonNote
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          margin: "20px",
+        }}
+      >
+        No more dreams
+      </IonNote>
+    );
   }
+  return data.map((dream) => <DreamCard key={dream.id} {...dream} />);
+}
 
-  const dreams = useSWR<Dream[]>(`${DREAMFLOW_API_URL}/users/${fetchUser().uid}/dreams`, fetcher).data || [];
-  const shownDreams = useMemo(() => filterDreams(textFilter, dreams), [textFilter, dreams]);
+export function MyDreams() {
+  const [page, setPage] = useState<number>(1);
+  const [textFilter, setTextFilter] = useState<string>("");
+  const [morePagesExist, setMorePagesExist] = useState<boolean>(true);
+
+  // const PAGE_SIZE = 10;
+
+  // function filterDreams(searchFilter: string, dreams: Dream[]) {
+  //   return dreams
+  //     .filter((dream) => {
+  //       var titleMatch = dream.title.toLowerCase().includes(searchFilter.toLowerCase());
+  //       var descriptionMatch = dream.description.toLowerCase().includes(searchFilter.toLowerCase());
+  //       return titleMatch || descriptionMatch;
+  //     })
+  //     .sort((a, b) => {
+  //       return new Date(a.date) < new Date(b.date) ? 1 : -1;
+  //     });
+  // }
 
   async function refreshDreams(event: CustomEvent) {
     mutate(`${DREAMFLOW_API_URL}/users/${fetchUser().uid}/dreams`);
     event.detail.complete();
+  }
+
+  async function loadMoreDreams(event: CustomEvent) {
+    console.log("Loading more!");
+    setPage(page + 1);
+    setTimeout(() => (event.target as HTMLIonInfiniteScrollElement).complete(), 500);
+  }
+
+  const pages = [];
+  for (let i = 1; i <= page; i++) {
+    pages.push(<DreamPage key={i} page={i} pageSize={10} setMorePagesExist={setMorePagesExist} />);
   }
 
   return (
@@ -56,9 +104,12 @@ export function MyDreams() {
         <IonRefresher slot="fixed" onIonRefresh={refreshDreams}>
           <IonRefresherContent />
         </IonRefresher>
-        {shownDreams.map((dream) => (
-          <DreamCard {...dream} key={dream.id} />
-        ))}
+        {pages}
+        {morePagesExist ? (
+          <IonInfiniteScroll threshold="100px" onIonInfinite={loadMoreDreams}>
+            <IonInfiniteScrollContent loadingText="Loading more dreams..."></IonInfiniteScrollContent>
+          </IonInfiniteScroll>
+        ) : null}
       </IonContent>
       <Link to="/dreams/add">
         <IonFab vertical="bottom" horizontal="end" aria-label="add dream">
